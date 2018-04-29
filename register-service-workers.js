406ee3c64bf72656697c2d9962a4962b51f9dfd9
Gal Akerman <30698827+akermang@users.noreply.github.com>
2018-04-29 13:21:58 +0300

    Update register-service-workers.js

--- register-service-workers.js
+++ register-service-workers.js
@@ -1,5 +1,5 @@
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js')
+    navigator.serviceWorker.register('https://github.com/akermang/eapi/sw.js')
     .then(function(reg) {
         // registration worked
         console.log('Registration succeeded. Scope is ' + reg.scope);
